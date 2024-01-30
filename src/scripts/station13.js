41483fee0f076018ad9efd20455095c59091f5a1
みのむし <mm1640052@gmail.com>
2024-01-30 18:46:04 +0900

    station13 clear

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,10 @@
 function changeBackgroundColor(e) {
+	text = document.getElementById("text");
   // ここに背景色を変える処理を書く。
-
+	if(check.checked){
+		text.style.backgroundColor = "red";
+	}
+	else{
+		text.style.backgroundColor = "white";
+	}
 }
